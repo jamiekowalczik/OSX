b0fe10aefa32e2f9b72277f867dff4f2b7a1b186
jamiekowalczik <jamie.kowalczik@gmail.com>
2015-08-12 12:47:13 -0400

    Update join_domain.sh

--- Scripts/join_domain.sh
+++ Scripts/join_domain.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# $4 - DOMAIN
+# $5 - BINDUSERNAME
+# $6 - BINDPASSWORD
+# $7 - OU
+# $8 - ADMINGROUPS
+# $9 - MOBILEHOME
+
 ## Use $2 or "`scutil --get ComputerName`" to obtain computername
 if [ "$9" -gt 0 ]; then
    dsconfigad -add "$4" -username "$5" -password "$6" -computer "$2" -mobile enable -mobileconfirm disable -localhome enable -useuncpath disable -shell /bin/bash -ou "$7" -groups "$8" -force >/dev/null 2>&1
