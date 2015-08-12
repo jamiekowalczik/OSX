0dbbdb7e93f811a1334fc14e953e04899a124d3f
jamiekowalczik <jamie.kowalczik@gmail.com>
2015-08-12 12:45:45 -0400

    Update join_domain.sh

--- Scripts/join_domain.sh
+++ Scripts/join_domain.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-## Use $2 instead of running scutil
-dsconfigad -add "$4" -username "$5" -password "$6" -computer "`scutil --get ComputerName`" -mobile enable -mobileconfirm disable -localhome enable -useuncpath disable -shell /bin/bash -ou "$7" -groups "$8" -force >/dev/null 2>&1
+## Use $2 or "`scutil --get ComputerName`" to obtain computername
+if [ "$9" -gt 0 ]; then
+   dsconfigad -add "$4" -username "$5" -password "$6" -computer "$2" -mobile enable -mobileconfirm disable -localhome enable -useuncpath disable -shell /bin/bash -ou "$7" -groups "$8" -force >/dev/null 2>&1
+else
+   dsconfigad -add "$4" -username "$5" -password "$6" -computer "$2" -localhome enable -useuncpath disable -shell /bin/bash -ou "$7" -groups "$8" -force >/dev/null 2>&1
+fi
 
 exit 0
